dfccb4303e06344be54890033c2440b02c791224
cyanes25 <cyanes@adacapital.com.br>
2023-05-10 00:46:42 -0300

    ajuste brabo de nomes

--- ios/brlaTests/reactNativeDemoTests.m
+++ ios/brlaTests/reactNativeDemoTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface reactNativeDemoTests : XCTestCase
+@interface brlaTests : XCTestCase
 
 @end
 
-@implementation reactNativeDemoTests
+@implementation brlaTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {

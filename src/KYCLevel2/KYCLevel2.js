047e9fee1a6b644f18175126461a2b725212ddfa
cyanes25 <cyanes@adacapital.com.br>
2023-06-05 10:37:34 -0300

    ajustes sdk

--- src/KYCLevel2/KYCLevel2.js
+++ src/KYCLevel2/KYCLevel2.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Button, View } from 'react-native';
 import IdwallSdk, {
   IdwallDocumentType,
@@ -6,38 +6,32 @@ import IdwallSdk, {
 } from '@idwall/react-native-idwall-sdk';
 import KYCPFLevel1 from '../KYCLevel1/KYCLevel1';
 
-IdwallSdk.initialize('102f2511-e55d-4f4a-ad53-ac56c0e79fde');
-if (IdwallSdk.ios) {
-  IdwallSdk.ios.setupPublicKeys(["AHYMQP+2/KIo32qYcfqnmSn+N/K3IdWallKey=", "tDilFQ4366PMdAmN/kyNiBQy24YHjuDs6Qsa6Oc/4c8="]);
-}
-
 const KYCPFLevel2 = () => {
   const [docType, setDocType] = useState(null);
 
-  const handleDocumentCapture = async () => {
+  useEffect(() => {
+    IdwallSdk.initialize('102f2511-e55d-4f4a-ad53-ac56c0e79fde');
+    if (IdwallSdk.ios) {
+      IdwallSdk.ios.setupPublicKeys(["AHYMQP+2/KIo32qYcfqnmSn+N/K3IdWallKey=", "tDilFQ4366PMdAmN/kyNiBQy24YHjuDs6Qsa6Oc/4c8="]);
+    }
+  }, []);
+
+  const handleRgCapture = async () => {
     try {
-      if (docType === 'RG') {
-        // Capture front and back for RG
-        await IdwallSdk.requestDocument(IdwallDocumentType.RG, IdwallDocumentSide.FRONT);
-        await IdwallSdk.requestDocument(IdwallDocumentType.RG, IdwallDocumentSide.BACK);
-      } else if (docType === 'CNH') {
-        // Capture only front for CNH
-        await IdwallSdk.requestDocument(IdwallDocumentType.CNH, IdwallDocumentSide.FRONT);
-      }
-
-      // Capture liveness
-      await IdwallSdk.requestLiveness();
-
-      // Submit document and liveness data
-      let token;
-      if (docType === 'RG') {
-        token = await IdwallSdk.sendRgWithLivenessData();
-      } else if (docType === 'CNH') {
-        token = await IdwallSdk.sendCnhWithLivenessData();
-      }
-
+      await IdwallSdk.requestDocument(IdwallDocumentType.RG, IdwallDocumentSide.FRONT);
+      await IdwallSdk.requestDocument(IdwallDocumentType.RG, IdwallDocumentSide.BACK);
+      const token = await IdwallSdk.sendRgWithLivenessData();
       console.log("Token: " + token);
+    } catch (error) {
+      console.log("Error: " + error);
+    }
+  };
 
+  const handleCnhCapture = async () => {
+    try {
+      await IdwallSdk.requestDocument(IdwallDocumentType.CNH, IdwallDocumentSide.FRONT);
+      const token = await IdwallSdk.sendCnhWithLivenessData();
+      console.log("Token: " + token);
     } catch (error) {
       console.log("Error: " + error);
     }
@@ -47,7 +41,8 @@ const KYCPFLevel2 = () => {
     <View>
       <Button title="Select RG" onPress={() => setDocType('RG')} />
       <Button title="Select CNH" onPress={() => setDocType('CNH')} />
-      {docType && <Button title="Start Capture" onPress={handleDocumentCapture} />}
+      {docType === 'RG' && <Button title="Start RG Capture" onPress={handleRgCapture} />}
+      {docType === 'CNH' && <Button title="Start CNH Capture" onPress={handleCnhCapture} />}
     </View>
   );
 };
